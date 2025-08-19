4aca8b049bacda3d19c71ff9f792f43f66a2175a
swarna5141 <swarnanimmagadda20@gmail.com>
2025-08-20 00:51:48 +0530

    applivation.java file added

---
+++ application.java
@@ -0,0 +1,11 @@
+ublic class SimpleMethod {
+    // Method to print a greeting
+    public void greet() {
+        System.out.println("Hello, Java!");
+    }
+
+    public static void main(String[] args) {
+        SimpleMethod obj = new SimpleMethod();
+        obj.greet(); // Calling the greet method
+    }
+}
